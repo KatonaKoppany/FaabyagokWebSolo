0a48d574cdf68cc45c069610fab52f37e44d8836
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-24 19:16:52 +0100

    Basic NavBar structure, CSS links

--- header.php
+++ header.php
@@ -4,6 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+
+        <nav>
+            <ul class="navUl">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="paint.php">Paints</a></li>
+                <li><a href="tools.php">Tools</a></li>
+                <li><a href="woodMetarials.php">Wood Materials</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="blog.php">Blog</a></li>
+            </ul>
+        </nav>
+
+    </header>
