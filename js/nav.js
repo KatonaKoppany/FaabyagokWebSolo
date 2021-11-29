355e01231a9ad9c7e60422552381ec5ad2453f5e
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2021-11-29 12:58:48 +0100

    Menu icon

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,18 @@
-let menuBtn = document.getElementById('navBTN');
+let navBtn = document.getElementById('navBTN');
 let nav = document.getElementsByClassName('nav')[0];
 let menuCkick = false;
 
-menuBtn.addEventListener('click', () => {
+navBtn.addEventListener('click', () => {
     const visibility = nav.getAttribute('data-visibles');
     console.log(visibility);
+    console.dir(navBtn);
     
     if(visibility === "false"){
         nav.setAttribute('data-visibles', true);
+        navBtn.children[0].classList.add('fa-times');
     }
     else if(visibility === "true"){
         nav.setAttribute('data-visibles', false);
+        navBtn.children[0].classList.remove('fa-times');
     }
 });
